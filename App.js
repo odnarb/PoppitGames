15b4cd2b4fb8cf07450a77f006aadd0135e3e24b
Brandon <bran.cham@gmail.com>
2020-01-29 21:14:25 -0700

    Cleaned up app.js

--- App.js
+++ App.js
@@ -1,12 +1,4 @@
 import React from 'react';
-import {
-  ActivityIndicator,
-  StatusBar,
-  StyleSheet,
-  View,
-  Text,
-  Button
-} from 'react-native';
 
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';

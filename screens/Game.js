48a0707d6c879327bc2969c9b8c34ca1b8af0d17
Brandon <bran.cham@gmail.com>
2020-02-24 21:05:45 -0700

    Game - Support calling a game, or url and getting info back from it.

--- screens/Game.js
+++ screens/Game.js
@@ -9,9 +9,40 @@ class GameScreen extends React.Component {
     header: null
   };
 
+//send this screen company_id, campaign_id and coupon id
+//coupon id has the campaign information regarding branding
+
+//get QuickTrip brand
+//get Subway Brand
+//get Pepsi Brand
+//get Coca-Cola brand
+
+  marker = this.props.navigation.getParam('current_marker');
+  user_id = this.props.navigation.getParam('user_id');
+  company_id = this.props.navigation.getParam('company_id');
+
+  _onMessage = (e) => {
+    let activityData = {};
+    try {
+      activityData = JSON.parse(e.nativeEvent.data);
+    } catch(e) {
+      //could not save callback data for campaign activity
+    }
+
+    activityData.user_id = this.user_id;
+    activityData.company_id = this.company_id;
+
+    console.log("--GOT MESSAGE FROM CAMPAIGN ACTIVITY: ", activityData);
+
+    this.props.navigation.navigate('Maps', activityData);
+  }
+
   render() {
+    let queryString = '?company_id=' + this.marker.company_id + '&campaign_id=' + this.marker.campaign_id;
     return (
-      <WebView source={{uri: 'http://bchambers.io/games/penalty-kicks/index.html'}} />
+      <WebView
+        onMessage={(e) => {this._onMessage(e)}}
+        source={{uri: this.marker.content_url + queryString }} />
     );
   }
 }

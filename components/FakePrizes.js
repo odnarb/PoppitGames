48774034073c242be64bfa4a388ac1c6212b271a
Brandon Chambers <bran.cham@gmail.com>
2020-05-12 21:25:46 -0700

    Barebones implementation of the prizes screen.

---
+++ components/FakePrizes.js
@@ -0,0 +1,130 @@
+const prizes = [
+{
+    company_id: 1,
+    campaign_id: 123,
+    hash: "111111111-12345-11111111111111",
+    title: "Quick Trip #1",
+    description: "Test description #1",
+    activity_state: "none", // none|completed|win|lose|entered
+    activity_state_detail: "none",
+    type: "game", // game | raffle | survey
+    coupon: {
+      title: "$.50 OFF",
+      description: "This is the coupon description."
+    },
+    image: { uri: "http://poppit.bchambers.io/assets/images/brands/quicktrip-logo-small.png" }
+},
+{
+    company_id: 1,
+    campaign_id: 124,
+    seen: false,
+    hash: "222222222-12345-22222222222222",
+    title: "Quick Trip #2",
+    description: "Test description #2",
+    activity_state: "none", // none|completed|win|lose|entered
+    activity_state_detail: "none",
+    type: "game", // game | raffle | survey
+    coupon: {
+      title: "-50% OFF",
+      description: "This is the coupon description."
+    },
+    image: { uri: "http://poppit.bchambers.io/assets/images/brands/quicktrip-logo-small.png" }
+},
+{
+    company_id: 1,
+    campaign_id: 125,
+    seen: false,
+    hash: "333333333-12345-33333333333333",
+    title: "Quick Trip #3",
+    description: "Test description #3",
+    activity_state: "none", // none|completed|win|lose|entered
+    activity_state_detail: "none",
+    type: "game", // game | raffle | survey
+    coupon: {
+      title: "FREE COFFEE",
+      description: "This is the coupon description."
+    },
+    image: { uri: "http://poppit.bchambers.io/assets/images/brands/quicktrip-logo-small.png" }
+},
+{
+    company_id: 1,
+    campaign_id: 126,
+    seen:false,
+    hash: "fde1d29f-fa88-4f01-b0c0-831cf3d4d028",
+    title: "Quick Trip #4",
+    description: "Test description #4",
+    activity_state: "none", // none|completed|win|lose|entered
+    activity_state_detail: "none",
+    type: "game", // game | raffle | survey
+    coupon: {
+        title: "Coupon #4",
+        description: "This is the coupon description."
+    },
+    image: { uri: "http://poppit.bchambers.io/assets/images/brands/quicktrip-logo-small.png" }
+},
+{
+    company_id: 1,
+    campaign_id: 127,
+    seen:false,
+    hash:"d16a55c2-12b9-443e-83ec-0934eba23274",
+    title:"Quick Trip #5",
+    description:"Test description #5",
+    activity_state: "none", // none|completed|win|lose|entered
+    activity_state_detail: "none",
+    type: "game", // game | raffle | survey
+    coupon:{
+        title:"Coupon #5",
+        description: "This is the coupon description."
+    },
+    image:{ uri: "http://poppit.bchambers.io/assets/images/brands/quicktrip-logo-small.png" }
+},
+{
+    company_id: 1,
+    campaign_id: 130,
+    seen:false,
+    hash:"395e577c-0f48-4c1e-a76c-1601d2256287",
+    title:"Quick Trip #8",
+    description:"Test description #8",
+    activity_state: "none", // none|completed|win|lose|entered
+    activity_state_detail: "none",
+    type: "game", // game | raffle | survey
+    coupon:{
+        title:"Coupon #8",
+        description: "This is the coupon description."
+    },
+    image:{ uri: "http://poppit.bchambers.io/assets/images/brands/quicktrip-logo-small.png" }
+},
+{
+    company_id: 1,
+    campaign_id: 131,
+    seen:false,
+    hash:"56716005-fe25-4426-90ea-2c74087a1b3c",
+    title:"Quick Trip #9",
+    description:"Test description #9",
+    activity_state: "none", // none|completed|win|lose|entered
+    activity_state_detail: "none",
+    type: "game", // game | raffle | survey
+    coupon: {
+        title:"Coupon #9",
+        description: "This is the coupon description."
+    },
+    image: { uri: "http://poppit.bchambers.io/assets/images/brands/quicktrip-logo-small.png" }
+},
+{
+    company_id: 1,
+    campaign_id: 132,
+    seen:false,
+    hash:"a3e0b227-dd90-4b31-ac50-fa421c749dae",
+    title:"Quick Trip #10",
+    description:"Test description #10",
+    activity_state: "none", // none|completed|win|lose|entered
+    activity_state_detail: "none",
+    type: "game", // game | raffle | survey
+    coupon: {
+        title:"Coupon #10",
+        description: "This is the coupon description."
+    },
+    image: { uri: "http://poppit.bchambers.io/assets/images/brands/quicktrip-logo-small.png" }
+}];
+
+export { prizes }
--- screens/Prizes.js
+++ screens/Prizes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import {
+  ActivityIndicator,
   ScrollView,
   Text,
   View,
@@ -12,12 +13,71 @@ import BottomNavigation from '../components/BottomNavigation';
 
 import { prizesStyleSheet as styles } from '../components/globalstyles';
 
+import { prizes as FAKE_PRIZES } from '../components/FakePrizes';
+
 class PrizesScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      prizes: []
+    };
+  }
+
   static navigationOptions = {
     //in newer versions this is the correct way to hide the title
     // headerShown: false
     header: null
   };
+
+/*
+              <View style={[styles.flatLightGreyBG,styles.prizeContainer]}>
+                <View style={styles.prizeCell}>
+                  <Text style={[styles.white,styles.prizeText]}>FREE{'\n'}2 Liter Pepsi</Text>
+                </View>
+
+                <View style={styles.prizeCell}>
+                  <Text style={[styles.white,styles.prizeText]}>Hooters{'\n'}Scottsdale</Text>
+                </View>
+
+                <View style={styles.prizeCell}>
+                  <Text style={[styles.white,styles.prizeText]}>CLAIMED</Text>
+                </View>
+              </View>
+*/
+
+  _renderPrizes = () => {
+    if( this.state.prizes.length == 0 ){
+      //show a loading spinner animation
+      return (<ActivityIndicator size="large" />);
+    }
+    return ( this.state.prizes.map( (prize, index) => {
+      return (
+        <View style={[styles.greenBG,styles.prizeContainer]} key={index}>
+          <View style={styles.prizeCell}>
+            <Text style={[styles.grey,styles.prizeText]}>-25% OFF{'\n'}2 Liter Pepsi</Text>
+          </View>
+
+          <View style={styles.prizeCell}>
+            <Text style={[styles.grey,styles.prizeText]}>Hooters{'\n'}Scottsdale</Text>
+          </View>
+
+          <View style={styles.prizeCell}>
+            <Text style={[styles.grey,styles.prizeText]}>CLAIM</Text>
+          </View>
+        </View>
+      )
+    }));
+  };
+
+  componentDidMount() {
+    //call to get the prizes from the server
+    setTimeout(() => {
+      this.setState({
+        prizes: FAKE_PRIZES
+      });
+    }, 1500 );
+  }
 
   render() {
     return (
@@ -30,184 +90,7 @@ class PrizesScreen extends React.Component {
             <Text style={[styles.grey,styles.padLeft20,styles.textHeader]}>My Prizes</Text>
 
             <ScrollView style={{}}>
-              <View style={[styles.greenBG,styles.prizeContainer]}>
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>-25% OFF{'\n'}2 Liter Pepsi</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>Hooters{'\n'}Scottsdale</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>CLAIM</Text>
-                </View>
-              </View>
-              <View style={[styles.greenBG,styles.prizeContainer]}>
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>-25% OFF{'\n'}2 Liter Pepsi</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>Hooters{'\n'}Scottsdale</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>CLAIM</Text>
-                </View>
-              </View>
-              <View style={[styles.greenBG,styles.prizeContainer]}>
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>-25% OFF{'\n'}2 Liter Pepsi</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>Hooters{'\n'}Scottsdale</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>CLAIM</Text>
-                </View>
-              </View>
-              <View style={[styles.greenBG,styles.prizeContainer]}>
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>-25% OFF{'\n'}2 Liter Pepsi</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>Hooters{'\n'}Scottsdale</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>CLAIM</Text>
-                </View>
-              </View>
-              <View style={[styles.greenBG,styles.prizeContainer]}>
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>-25% OFF{'\n'}2 Liter Pepsi</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>Hooters{'\n'}Scottsdale</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>CLAIM</Text>
-                </View>
-              </View>
-
-              <View style={[styles.greenBG,styles.prizeContainer]}>
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>-25% OFF{'\n'}2 Liter Pepsi</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>Hooters{'\n'}Scottsdale</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>CLAIM</Text>
-                </View>
-              </View>
-
-              <View style={[styles.greenBG,styles.prizeContainer]}>
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>-25% OFF{'\n'}2 Liter Pepsi</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>Hooters{'\n'}Scottsdale</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.grey,styles.prizeText]}>CLAIM</Text>
-                </View>
-              </View>
-
-
-              <View style={[styles.flatLightGreyBG,styles.prizeContainer]}>
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.white,styles.prizeText]}>FREE{'\n'}2 Liter Pepsi</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.white,styles.prizeText]}>Hooters{'\n'}Scottsdale</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.white,styles.prizeText]}>CLAIMED</Text>
-                </View>
-              </View>
-
-              <View style={[styles.flatLightGreyBG,styles.prizeContainer]}>
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.white,styles.prizeText]}>FREE{'\n'}2 Liter Pepsi</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.white,styles.prizeText]}>Hooters{'\n'}Scottsdale</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.white,styles.prizeText]}>CLAIMED</Text>
-                </View>
-              </View>
-
-              <View style={[styles.flatLightGreyBG,styles.prizeContainer]}>
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.white,styles.prizeText]}>FREE{'\n'}2 Liter Pepsi</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.white,styles.prizeText]}>Hooters{'\n'}Scottsdale</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.white,styles.prizeText]}>CLAIMED</Text>
-                </View>
-              </View>
-
-              <View style={[styles.flatLightGreyBG,styles.prizeContainer]}>
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.white,styles.prizeText]}>FREE{'\n'}2 Liter Pepsi</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.white,styles.prizeText]}>Hooters{'\n'}Scottsdale</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.white,styles.prizeText]}>CLAIMED</Text>
-                </View>
-              </View>
-
-              <View style={[styles.flatLightGreyBG,styles.prizeContainer]}>
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.white,styles.prizeText]}>FREE{'\n'}2 Liter Pepsi</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.white,styles.prizeText]}>Hooters{'\n'}Scottsdale</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.white,styles.prizeText]}>CLAIMED</Text>
-                </View>
-              </View>
-
-              <View style={[styles.flatLightGreyBG,styles.prizeContainer]}>
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.white,styles.prizeText]}>FREE{'\n'}2 Liter Pepsi</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.white,styles.prizeText]}>Hooters{'\n'}Scottsdale</Text>
-                </View>
-
-                <View style={styles.prizeCell}>
-                  <Text style={[styles.white,styles.prizeText]}>CLAIMED</Text>
-                </View>
-              </View>
+              {this._renderPrizes()}
             </ScrollView>
           </View>
           <BottomNavigation />
